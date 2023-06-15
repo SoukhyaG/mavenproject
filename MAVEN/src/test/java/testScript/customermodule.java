4f555f6918af4a3eb2d67f055fbe6f77415971d5
deeks <deeks@Deekshitha>
2023-06-15 12:06:00 +0530

    1 commit

---
+++ MAVEN/src/test/java/testScript/customermodule.java
@@ -0,0 +1,11 @@
+package testScript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class customermodule {
+@Test
+public void customermodule() {
+	Reporter.log("customer module",true);
+}
+}
